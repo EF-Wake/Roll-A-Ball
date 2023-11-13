63aee01a577063e8517eb4720f17a542257658fa
EF-Wake <elfisher2@my.waketech.edu>
2023-11-13 09:47:06 -0500

    Roll A Ball

---
+++ RollABallFisher/Assets/scripts/Tower.cs
@@ -0,0 +1,30 @@
+//////////////////////////////////////////////////////
+// Assignment/Lab/Project: Roll A Ball
+//Name: Ethan Fisher
+//Section: 2023FA.SGD.113.2102
+//Instructor: Ven Lewis
+// Date: 11/8/2023
+//////////////////////////////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    GameObject Projectile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    /*public invokeRepeating()
+    {
+
+        return;
+    }*/
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
